447b631e38aac32836781bf6bbbfa4e5e2ef2529
ofindik <osman.findik@gmail.com>
2023-08-06 15:50:41 +0300

    profile samples

--- src/main/java/dev/osmanfindik/contentcalendar/config/DataLoader.java
+++ src/main/java/dev/osmanfindik/contentcalendar/config/DataLoader.java
@@ -1,8 +1,10 @@
 package dev.osmanfindik.contentcalendar.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+//@Profile("!dev")
 //@Component
 public class DataLoader implements CommandLineRunner {
 	@Override
